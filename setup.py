a086bde3c49d079686f22acf55a83609bef6980f
Lucas Rangel Cezimbra <lucas.cezimbra@gmail.com>
2022-08-18 18:55:04 -0300

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -38,6 +38,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/HBN3tw0rk/dcache",
-    version="0.0.1",
+    version="0.0.2",
     zip_safe=False,
 )
